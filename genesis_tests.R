95f257e700427c6e00610af20b33223774f4801a
tmajaria <tmajaria@broadinstitute.org>
2019-09-26 12:15:52 -0400

    tests outfile name suffix to .csv

--- genesis_tests.R
+++ genesis_tests.R
@@ -303,6 +303,7 @@ runMainAnalysis = function(user_cores,in_nullmod,keepVars=NULL, keepGenes=NULL){
 
 #sort( sapply(ls(),function(x){object.size(get(x))})) 
 results = as.data.frame(runMainAnalysis(user_cores=user_cores,in_nullmod=nullmod),stringsAsFactors=F)
+if (!endsWith(results.file, ".csv")) results.file <- paste0(results.file, ".csv")
 results.file <- sub(".csv", paste0(".", chr, ".csv"), results.file)
 if (!endsWith(results.file, ".gz")) results.file <- paste0(results.file, ".gz")
 out <- gzfile(results.file, "w")
